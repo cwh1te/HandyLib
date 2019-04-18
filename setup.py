334bf08f1b472a2dbc832171370bd6a12c05ed7f
Caleb White <cmwhite@protonmail.com>
2019-04-18 16:34:43 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='HandyLib',
-    packages='HandyLib',
+    packages=find_packages(),
     url='https://github.com/cwh1te/HandyLib',
     description='A collection of handy Python functions',
     long_description=open('README.md').read(),
-    install_requires=open('requirements.txt).readlines(),
+    install_requires=open('requirements.txt').readlines(),
     include_package_data=True,
 )
