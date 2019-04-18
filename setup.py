aa68617141436131861b35b23e9ef94f58084196
Caleb White <cmwhite@protonmail.com>
2019-04-18 16:01:35 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='HandyLib',
+    packages=find_packages(),
+    url='https://github.com/cwh1te/HandyLib',
+    description='A collection of handy Python functions',
+    long_description=open('README.md').read(),
+    install_requires=open('requirements.txt).readlines(),
+    include_package_data=True,
+)
